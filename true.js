89f3957a2a46e5378bd966fe98cc1753f4862f7e
hiroya-shimomoto <68986119+hiroya-shimomoto@users.noreply.github.com>
2020-07-30 15:40:27 +0900

    Add files via upload

---
+++ true.js
@@ -0,0 +1,36 @@
+function true1()
+{
+    var doc0= document.getElementById("a");  
+    doc0.innerHTML= "正解！！";
+}
+
+function false1()
+{
+     var doc0= document.getElementById("a");  
+    doc0.innerHTML= "残念！正解は子どもです";
+}
+
+function true2()
+{
+    var doc0= document.getElementById("b");  
+    doc0.innerHTML= "正解！！";
+}
+
+function false2()
+{
+     var doc0= document.getElementById("b");  
+    doc0.innerHTML= "残念！正解はリフレインです";
+}
+
+function true3()
+{
+    var doc0= document.getElementById("c");  
+    doc0.innerHTML= "正解！！";
+}
+
+function false3()
+{
+     var doc0= document.getElementById("c");  
+    doc0.innerHTML= "残念！正解はリラックスです";
+}
+
